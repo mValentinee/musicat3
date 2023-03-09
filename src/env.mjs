6c2145075bdabd0d30d756e54c094fa3590ebe9c
Matthew Valentine <108686495+mValentinee@users.noreply.github.com>
2023-03-09 17:27:23 -0500

    chained .optional() to database_url

--- src/env.mjs
+++ src/env.mjs
@@ -5,7 +5,7 @@ import { z } from "zod";
  * built with invalid env vars.
  */
 const server = z.object({
-  DATABASE_URL: z.string().url(),
+  DATABASE_URL: z.string().url().optional(),
   NODE_ENV: z.enum(["development", "test", "production"]),
 });
 
